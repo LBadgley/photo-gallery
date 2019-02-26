d75a3c6696bbea3dbbff0ec7460f5c403a3328f7
Laura Badgley <l.badgleyy@gmail.com>
2019-02-25 18:16:43 -0800

    building html scaffolding building tests

---
+++ src/index.js
@@ -0,0 +1,30 @@
+body {
+    font-family: "Segoe UI", Candara, "Bitstream Vera Sans", "DejaVu Sans", "Bitstream Vera Sans", "Trebuchet MS", Verdana, "Verdana Ref", sans-serif;
+}
+
+* {
+    box-sizing: border-box;
+}
+
+header {
+    display: flex;
+    align-items: center;
+}
+
+h1 {
+    margin-left: 20px;
+}
+
+#the-thing {
+    flex-direction: column;
+}
+#images-list li {
+    list-style: none;
+    border: 1px solid black;
+    width: 400px;
+    margin: 40px;
+}
+#images-list img {
+    height: 200px;
+    padding: 10px;
+}
---
+++ test/images-template.test.js
@@ -0,0 +1,17 @@
+const test = QUnit.test;
+
+QUnit.module('template creation test');
+
+test('create a template to pull img data from dom', function(assert) {
+    // arrange
+
+    // act
+
+
+    // assert
+    assert.htmlEqual(result, /*html*/ `
+        
+    
+    
+    `)
+});
